544b45f2898046a7e43fd583d3ba074cd88f3baf
Aivar Annamaa <aivar.annamaa@gmail.com>
2018-02-19 23:39:43 +0200

    init

---
+++ thonnycontrib/calliope/__init__.py
@@ -0,0 +1,15 @@
+from thonnycontrib.micropython import MicroPythonProxy, MicroPythonConfigPage
+from thonny.globals import get_workbench
+
+class CalliopeMiniProxy(MicroPythonProxy):
+    @property
+    def firmware_filetypes(self):
+        return [('*.hex files', '.hex'), ('all files', '.*')]
+
+class CalliopeMiniConfigPage(MicroPythonConfigPage):
+    pass
+
+def load_early_plugin():
+    get_workbench().add_backend("CalliopeMini", CalliopeMiniProxy, 
+                                "MicroPython on Calliope mini", CalliopeMiniConfigPage)
+
