197dc50fc9f3a99165691b5c0a82d8bfe69a08b5
Aivar Annamaa <aivar.annamaa@gmail.com>
2018-05-23 20:33:38 +0300

    firmware uploading

--- thonnycontrib/calliope/__init__.py
+++ thonnycontrib/calliope/__init__.py
@@ -1,7 +1,9 @@
+import os.path
 from thonnycontrib.micropython import MicroPythonProxy, MicroPythonConfigPage
 from thonny import get_workbench
-from thonny.running import create_frontend_python_process
-from thonny.ui_utils import SubprocessDialog
+from thonny.ui_utils import run_with_busy_window
+from thonny.misc_utils import find_volume_by_name
+import shutil
 
 class CalliopeMiniProxy(MicroPythonProxy):
     pass
@@ -10,9 +12,18 @@ class CalliopeMiniConfigPage(MicroPythonConfigPage):
     pass
 
 def flash_the_firmware():
-    proc = create_frontend_python_process(['-u', '-m', 'uflash'])
-    dlg = SubprocessDialog(get_workbench(), proc, "Uploading firmware", autoclose=False)
-    dlg.wait_window()
+    mount_path = find_volume_by_name("MINI",
+                                     not_found_msg="Could not find disk '%s'.\n"
+                                        + "Make sure you have Calliope Mini plugged in!\n\n"
+                                        + "Do you want to continue and locate the disk yourself?")
+    if mount_path is None:
+        return
+    
+    hex_path = os.path.join(os.path.dirname(__file__), "res", "firmware.hex")
+    def work():
+        shutil.copy(hex_path, mount_path)
+    
+    run_with_busy_window(work, description="Copying firmware")
 
 def load_plugin():
     get_workbench().set_default("CalliopeMini.port", "auto")
