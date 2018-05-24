d2628611fc4e917917b99064beb3d9641bc8de38
Aivar Annamaa <aivar.annamaa@gmail.com>
2018-05-24 08:36:44 +0300

    Firmware uploading

--- thonnycontrib/calliope/__init__.py
+++ thonnycontrib/calliope/__init__.py
@@ -1,7 +1,7 @@
 import os.path
 from thonnycontrib.micropython import MicroPythonProxy, MicroPythonConfigPage
 from thonny import get_workbench
-from thonny.ui_utils import run_with_busy_window
+from thonny.ui_utils import FileCopyDialog
 from thonny.misc_utils import find_volume_by_name
 import shutil
 
@@ -11,7 +11,7 @@ class CalliopeMiniProxy(MicroPythonProxy):
 class CalliopeMiniConfigPage(MicroPythonConfigPage):
     pass
 
-def flash_the_firmware():
+def flash_the_firmware(hex_path):
     mount_path = find_volume_by_name("MINI",
                                      not_found_msg="Could not find disk '%s'.\n"
                                         + "Make sure you have Calliope Mini plugged in!\n\n"
@@ -19,17 +19,24 @@ def flash_the_firmware():
     if mount_path is None:
         return
     
-    hex_path = os.path.join(os.path.dirname(__file__), "res", "firmware.hex")
-    def work():
-        shutil.copy(hex_path, mount_path)
+    destination_path = os.path.join(mount_path, os.path.basename(hex_path))
     
-    run_with_busy_window(work, description="Copying firmware")
+    dlg = FileCopyDialog(get_workbench(), hex_path, destination_path, 
+                   "Uploading %s to %s" % (os.path.basename(hex_path), mount_path))
+    dlg.start_and_wait()
 
 def load_plugin():
     get_workbench().set_default("CalliopeMini.port", "auto")
     get_workbench().add_backend("CalliopeMini", CalliopeMiniProxy, 
                                 "MicroPython on Calliope mini", CalliopeMiniConfigPage)
 
-    get_workbench().add_command("uploadmicropythoncalliope", "tools", "Upload MicroPython to Calliope Mini ...",
-                                flash_the_firmware,
-                                group=120)
+    firmware_dir = os.path.join(os.path.dirname(__file__), "firmware")
+    for name in sorted(os.listdir(firmware_dir)):
+        if name.endswith(".hex"):
+            
+            def action(hex_path=os.path.join(firmware_dir, name)):
+                flash_the_firmware(hex_path)
+                 
+            get_workbench().add_command("uploadmicropythoncalliope" + name, "tools",
+                                    "Upload %s to Calliope Mini" % name[:-4].replace("_", " "),
+                                    action, group=120)
